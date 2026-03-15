d6d6a80ac37fd8a4fa40f7c4c05e52d195e3b20f
livin varghese <livinchakko91@gmail.com>
2026-03-15 16:45:56 +0530

    model training added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-# from src.cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 # from src.cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
@@ -33,16 +33,16 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Training"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    model_trainer = ModelTrainingPipeline()
-#    model_trainer.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
